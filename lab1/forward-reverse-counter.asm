29fb50bd6719b91f953886e7cd52dc1053bea225
vasconcelosdavi <davi.muniz41@gmail.com>
2021-08-21 19:58:47 -0300

    Add increase-decrease operation

--- lab1/forward-reverse-counter.asm
+++ lab1/forward-reverse-counter.asm
@@ -5,29 +5,29 @@
 ;**   Target uC: Atmel ATmega328P                                   **
 ;**   X-TAL frequency: 16 MHz                                       **
 ;**   AVRASM: AVR macro assembler 2.1.57							**
-;**			  (build 16 Aug 27 2014 16:39:43)						**
+;**			  (build 16 Aug 27 2014 16:39:43)                		**
 ;**                                                                 **
 ;**  Created: 2021/08/20 by Davi Muniz Vasconcelos                  **
 ;*********************************************************************
 ;*********************************************************************
 
-   .EQU BAUD_RATE_57600 = 16  ; Constant for baud rate
-   .EQU ASCII_D = 68  ; D letter in ASCII code
-   .EQU ASCII_I = 73  ; I letter in ASCII code
-   .EQU MIN_COUNTER_VALUE = 0 ; Minimum counter value
-   .EQU MAX_COUNTER_VALUE = 15 ; Maximum counter value
+   .EQU BAUD_RATE_57600 = 16      ; Constant for baud rate
+   .EQU ASCII_D = 68              ; D letter in ASCII code
+   .EQU ASCII_I = 73			  ; I letter in ASCII code
+   .EQU MIN_COUNTER_VALUE = 0	  ; Minimum counter value
+   .EQU MAX_COUNTER_VALUE = 15	  ; Maximum counter value
 
-   .CSEG			; FLASH segment code
-   .ORG 0			; entry point after POWER/RESET
+   .CSEG		; FLASH segment code
+   .ORG 0		; entry point after POWER/RESET
 	JMP RESET
 
    .ORG 0x100
 
 RESET:
-	LDI R16, LOW(0x8FF) ; Sets Stack Pointer to RAMEND
-	OUT SPL, R16
-	LDI R16, HIGH(0x8FF)
-	OUT SPH, R16
+	LDI R16, LOW(0x8FF)	    ; Sets Stack Pointer to RAMEND
+	OUT SPL, R16			;
+	LDI R16, HIGH(0x8FF)	;
+	OUT SPH, R16			;
 	
 	CALL USART_INIT  ; Goes to USART initialization code
 	CALL PORTD_INIT  ; Goes to PORTD initialization code
@@ -40,26 +40,28 @@ RESET:
 	LDI  ZL, LOW(2*MSG_DEC)
 	CALL SEND_MSG
 
-	LDI R18,0x00  ; Set counter decrease mode to zero
-	STS DECREASE_MODE, R18
-
+	LDI R20, 0x00  ; R20 stores the current counter value
+	LDI R21, 0x00  ; R21 stores the current counter mode (increase 0x00, decrease 0x01)
+	
 	LDI ZH, HIGH(2*MSG_DEFAULT_MODE)  ; Prints a message that specifies the default counter mode
 	LDI ZL, LOW(2*MSG_DEFAULT_MODE)
 	CALL SEND_MSG
 
-READ_TO_GO:                       ; Waits for open switch to start couting
-	IN   R16, PIND                ;
-    ANDI R16, 0b00000100          ;
-    BREQ READ_TO_GO               ;
+READ_TO_GO:				   ; Waits for open switch to start couting
+	IN   R16, PIND         ;
+    ANDI R16, 0b00000100   ;
+    BREQ READ_TO_GO        ;
 
-MAIN_LOOP:
-	CALL USART_RECEIVE ; Receives counter mode instruction given by terminal
+CHECK_TERM:
+	CALL USART_RECEIVE     ; Receives counter mode instruction given by terminal
+	CALL SET_COUNTER_MODE  ; Changes counter mode if the user request
 
+CHECK_PIN_STATE:
 	IN	 R19, PIND
 	SBRS R19, 2
 	CALL WAIT_SWITCH_RELEASE
 
-	JMP  MAIN_LOOP
+	JMP  CHECK_TERM
 
 ;*********************************************************************
 ;  Subroutine WAIT_SWITCH_RELEASE
@@ -70,52 +72,77 @@ WAIT_SWITCH_RELEASE:
 	BREQ WAIT_SWITCH_RELEASE
 
 UPDATE_COUNTER:
-	CALL USART_TRANSMIT
-	; CPI  DECREASE_MODE, 0x00
-	; CALL INC_COUNTER
+	SBRS R21, 0
+	CALL INC_COUNTER
 
-	; CPI  DECREASE_MODE, 0x01
-	; CALL DEC_COUNTER 
+	SBRC R21, 0
+	CALL DEC_COUNTER 
 	RET
 
 ;*********************************************************************
 ;  Subroutine INC_COUNTER
 ;*********************************************************************
+INC_COUNTER:
+	INC  R20
+	SWAP R20
+	OUT  PORTD, R20
+	SWAP R20
+	RET
+
+;*********************************************************************
+;  Subroutine DEC_COUNTER
+;*********************************************************************
+DEC_COUNTER:
+	DEC  R20
+	SWAP R20
+	OUT  PORTD, R20
+	SWAP R20
+	RET
 
 ;*********************************************************************
 ;  Subroutine SET_COUNTER_MODE
 ;*********************************************************************
 SET_COUNTER_MODE:
 	CPI  R16, ASCII_D
-	BREQ SET_COUNTER_DEC_MODE
+	BREQ PRESS_D
 
 	CPI  R16, ASCII_I
-	BREQ SET_COUNTER_INC_MODE
+	BREQ PRESS_I
+	RET
+
+PRESS_D:
+	CPI  R21, 0x01
+	BRNE SET_COUNTER_DEC_MODE
+	RET
+
+PRESS_I:
+	CPI  R21, 0x00
+	BRNE SET_COUNTER_INC_MODE
 	RET
 
 ;*********************************************************************
 ;  Subroutine SET_COUNTER_INC_MODE
 ;*********************************************************************
 SET_COUNTER_INC_MODE:
-	LDI  R18, 0x00 ; 0x00 flag indicates increase mode
-	STS  DECREASE_MODE, R18
+	LDI  R21, 0x00						  ; 0x00 flag means increase mode
 
 	LDI  ZH, HIGH(2*MSG_INC_CONFIRMATION) ; Prints confirmation message of increase mode
 	LDI  ZL, LOW(2*MSG_INC_CONFIRMATION)
 	CALL SEND_MSG
-	RET
+	
+	JMP CHECK_PIN_STATE
 
 ;*********************************************************************
 ;  Subroutine SET_COUNTER_DEC_MODE
 ;*********************************************************************
 SET_COUNTER_DEC_MODE:
-	LDI  R18, 0x01 ; 0x01 flag indicates decrease mode
-	STS  DECREASE_MODE, R18
+	LDI  R21, 0x01						  ; 0x01 flag means decrease mode
 
 	LDI  ZH, HIGH(2*MSG_DEC_CONFIRMATION) ; Prints confirmation message of decrease mode
 	LDI  ZL, LOW(2*MSG_DEC_CONFIRMATION)
 	CALL SEND_MSG
-	RET
+	
+	JMP CHECK_PIN_STATE
 
 ;*********************************************************************
 ;  Subroutine PORTD_INIT
@@ -125,7 +152,7 @@ PORTD_INIT:
 	LDI R18, 0b11110000 ; Set D7-D4 to output and D3-D0 to input
 	OUT DDRD, R18
 	
-	LDI R18, 0x00 ; Set counter value to zero
+	LDI R18, 0x00		; Set counter value to zero
 	OUT PORTD, R18
 	RET
 
@@ -154,15 +181,15 @@ USART_INIT:
 ;  Transmits (TX) R16   
 ;*********************************************************************
 USART_TRANSMIT:
-	PUSH R17                     ; Saves R17 into stack
+	PUSH R17              ; Saves R17 into stack
 
 WAIT_TRANSMIT:
 	LDS  R17, UCSR0A
-	SBRS R17, UDRE0		         ; Waits for TX buffer to get empty
+	SBRS R17, UDRE0		  ; Waits for TX buffer to get empty
 	RJMP WAIT_TRANSMIT
-	STS	 UDR0, R16	             ; Writes data into the buffer
+	STS	 UDR0, R16	      ; Writes data into the buffer
 
-	POP	R17                      ; Restores R17
+	POP	R17               ; Restores R17
 	RET
 
 ;*********************************************************************
@@ -170,13 +197,13 @@ WAIT_TRANSMIT:
 ;  Receives the char from USART and places it in the register R16 
 ;*********************************************************************
 USART_RECEIVE:
-	PUSH	R17                 ; Saves R17 into stack
+	PUSH R17			   ; Saves R17 into stack
 
 	LDS	 R17, UCSR0A
 	SBRC R17, RXC0
-	LDS  R16, UDR0		        ; Reads the data
+	LDS  R16, UDR0		   ; Reads the data
 
-	POP	R17                     ; Restores R17
+	POP	 R17               ; Restores R17
 	RET
 
 ;*********************************************************************
@@ -210,14 +237,5 @@ MSG_INC_CONFIRMATION:
 	.DB ":: Counter mode set to increase", 0x0A, 0x0D, '$'
 MSG_DEC_CONFIRMATION:
 	.DB ":: Counter mode set to decrease", 0x0A, 0x0D, '$'
-
-;*********************************************************************
-; Data segment (RAM)
-;*********************************************************************
-.DSEG
-	.ORG 0x200
-
-DECREASE_MODE:
-	.BYTE 1
 
 	.EXIT
