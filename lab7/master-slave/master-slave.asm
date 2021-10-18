310101077b37623bf13e5f04ee5c7a0de85f4d25
vasconcelosdavi <davi.muniz41@gmail.com>
2021-10-18 18:55:27 -0300

    add servo motor control

--- lab7/master-slave/master-slave.asm
+++ lab7/master-slave/master-slave.asm
@@ -15,6 +15,10 @@
    .EQU O_KEY = 79             ; O ASCII code
    .EQU N_KEY = 78             ; N ASCII code
    .EQU F_KEY = 70             ; F ASCII code
+
+   .EQU PERIOD_COUNT = 40000   ; time constant for 20ms
+   .EQU DEFAULT_COUNT = 2999   ; default count constant for zero degrees
+
    .EQU ACK_CODE = 0           ; ack code sent by slave to master
 
    .CSEG
@@ -39,6 +43,10 @@ RESET:
 	LDI R22, 0x00     ; stores fifth protocol character
 	LDI R24, 0x00     ; stores angle first digit
 	LDI R25, 0x00     ; stores angle second digit
+
+	LDI R26, 0x00     ; stores the high part of count (16-bit)
+	LDI R27, 0x00     ; stores the low part of count (16-bit)
+	LDI R28, 0x00     ; stores the angle (8-bit)
 	
 	LDI R23, 0x00     ; stores number of characters in a line of master's terminal
 
@@ -102,7 +110,9 @@ MASTER_INVALID:
 	JMP  MASTER_LOOP
 
 SLAVE:
-    LDI  ZH, HIGH(2*MSG_SLAVE)  ; sends slave identification message to terminal
+    CALL TIMER1_INIT_MODE14
+    CALL INIT_PORTB
+	LDI  ZH, HIGH(2*MSG_SLAVE)  ; sends slave identification message to terminal
 	LDI  ZL, LOW(2*MSG_SLAVE)   ;
 	CALL SEND_MSG_USART0        ;
 
@@ -286,7 +296,114 @@ SLAVE_LED_1_ON:
 	JMP SLAVE_READ_FIRST
 
 SLAVE_SUCCESS_SERVO:
-    JMP SLAVE_READ_FIRST
+    CALL SLAVE_GET_COUNT       ; evaluates the count for the given angle
+
+    CPI  R19, 0x30             ; if the servo number is 0, then changes the OCR1 of servo 0  
+    BREQ SLAVE_CHANGE_SERVO_0  ;
+
+    CPI  R19, 0x31             ; if the servo number is 1, then changes the OCR1 of servo 1
+    BREQ SLAVE_CHANGE_SERVO_1  ;
+
+    CPI  R19, 0x32             ; if the servo number is 2, then changes the OCR1 of servo 2
+    BREQ SLAVE_CHANGE_SERVO_2  ;
+
+SLAVE_CHANGE_SERVO_0:          ; changes the OCR1 value of servo 0
+    STS OCR1AH, R26            ;
+    STS OCR1AL, R27            ;
+    JMP SLAVE_READ_FIRST       ; starts a new protocol reading
+
+SLAVE_CHANGE_SERVO_1:          ; changes the OCR1 value of servo 1
+    STS OCR1BH, R26            ;
+    STS OCR1BL, R27            ;
+    JMP SLAVE_READ_FIRST       ; starts a new protocol reading
+
+SLAVE_CHANGE_SERVO_2:          ; changes the OCR1 value of servo 2
+    STS OCR1CH, R26            ;
+    STS OCR1CL, R27            ;
+    JMP SLAVE_READ_FIRST       ; starts a new protocol reading
+
+;************************************************************
+; Subroutine GET_COUNT                                     **
+; Gets the timer count (OCR1) for a given angle             **
+;************************************************************
+SLAVE_GET_COUNT:
+    PUSH R16     ; Saves R16 into stack
+
+    ADD R28, R25 ; Sums the unit angle digit to R25
+	
+    LDI R16, 10  ; Gets the tens of the angle 
+    MUL R24, R16 ;
+    ADD R28, R0  ;
+
+    CPI  R20, PLUS_KEY         ; Branch if the angle is positive
+    BREQ SLAVE_GET_PLUS_COUNT  ;
+
+    CPI  R20, MINUS_KEY        ; Branch if the angle is negative
+    BREQ SLAVE_GET_MINUS_COUNT ;
+
+;************************************************************
+; Subroutine GET_PLUS_COUNT                                **
+; Gets the timer count (OCR1) for a positive angle         **
+;************************************************************
+SLAVE_GET_PLUS_COUNT:             ; Gets the count for a plus sign angle 
+    LDI R26, HIGH(DEFAULT_COUNT)  ;
+    LDI R27, LOW(DEFAULT_COUNT)   ;
+
+    CPI  R28, 0                         ; Does not evaluate the count if the angle is zero
+    BREQ SLAVE_RETURN_PLUS_MULTIPLY_11  ;
+
+SLAVE_PLUS_MULTIPLY_11:          ; Iteratively multiplies by 11 the given angle and adds to DEFAULT_COUNT, since the integer part of (4000 - 2000) / 180 is 11
+    LDI R16, 11                  ;
+    ADD R27, R16                 ; Adds 11 to the low part of the count
+    LDI R16, 0                   ;
+    ADC R26, R16                 ; Adds a carry to the high part of the count, if any
+
+    DEC R28                     ; Decreases the given angle
+
+    CPI  R28, 0                 ; Repeats until the given angle is zero
+    BRNE SLAVE_PLUS_MULTIPLY_11 ;
+
+    MOV R16, R24           ; Loads the angle first digit into R16
+    INC R16                ;
+    ADD R27, R16           ; Sums additional value in the low part of the count to reduce angle error
+    LDI R16, 0             ; 
+    ADC R26, R16           ; Adds a carry in the high part of the count, if any 
+
+SLAVE_RETURN_PLUS_MULTIPLY_11:  ; Finishes the evaluation of the count for a given positive angle 
+    POP R16                     ; Restores R16
+    RET
+
+;************************************************************
+; Subroutine GET_MINUS_COUNT                               **
+; Gets the timer count (OCR1) for a negative angle         **
+;************************************************************
+SLAVE_GET_MINUS_COUNT:            ; Gets the count for a minus sign angle
+    LDI R26, HIGH(DEFAULT_COUNT)  ;
+    LDI R27, LOW(DEFAULT_COUNT)   ;
+
+    CPI  R28, 0                         ; Does not evaluate the count if the angle is zero
+    BREQ SLAVE_RETURN_MINUS_MULTIPLY_11 ;
+
+SLAVE_MINUS_MULTIPLY_11:   ; Iteratively multiplies by 11 the given angle and subtracts of DEFAULT_COUNT, since the integer part of (4000 - 2000) / 180 is 11
+    LDI R16, 11            ;
+    SUB R27, R16           ; Subtracts 11 of the count low part
+    LDI R16, 0             ;
+    SBC R26, R16           ; Subtracts a carry of the count high part, if any
+
+    DEC R28                ; Decreases the given angle
+
+    CPI  R28, 0                  ; Repeats until the given angle is zero
+    BRNE SLAVE_MINUS_MULTIPLY_11 ;
+
+    SUB  R27, R24          ; Subtracts the angle first digit of the count low part to reduce angle error
+    LDI  R16, 0            ; 
+    SBC  R26, R16          ; Subtracts a carry of the count high part, if any
+    SUBI R27, 1            ; Subtracts 1 of the count low part to reduce angle error
+    SBC  R26, R16          ; Subtracts a carry of the count high part, if any
+
+SLAVE_RETURN_MINUS_MULTIPLY_11:  ; Finishes the evaluation of the count for a given negative angle
+    POP R16                      ; Restores R16
+    RET
 
 SLAVE_UNSUCCESS_1:
     CALL USART1_RECEIVE
@@ -406,6 +523,15 @@ SECOND_DIGIT_IS_DIGIT:
 
 DIGIT_TRUE: ; returns to the next step of protocol reading
     RET     ; 
+
+;*********************************************************************
+;  Subroutine INIT_PORTB
+;  Sets PB5, PB6, and PB7 as outputs
+;*********************************************************************
+INIT_PORTB:
+    LDI R16, 0b11100000
+    OUT DDRB, R16
+    RET
 
 ;*********************************************************************
 ;  Subroutine INIT_PORTF
@@ -554,6 +680,49 @@ SEND_MSG_LOOP:
 
 END_SEND_MSG:
     POP	R16
+    RET
+
+;**************************************
+; Subroutine TIMER1_INIT_MODE14      **
+; Initializes timer 1 in mode 14     **
+; ICR = 40000 (20ms)                 **
+; 0CR1A = 2999 (0 degrees)           **
+; OCR1B = 2999 (0 degrees)           **
+; OCR1C = 2999 (0 degrees)           **
+; PRESCALER/8                        **
+;**************************************
+TIMER1_INIT_MODE14:
+; ICR1 = 40000
+    LDI R16, HIGH(PERIOD_COUNT)
+    STS ICR1H, R16
+    LDI R16, LOW(PERIOD_COUNT)
+    STS ICR1L, R16
+
+; OCR1A, OCR1B, OCR1C = 2999
+    LDI R16, DEFAULT_COUNT>>8
+    STS OCR1AH, R16
+    STS OCR1BH, R16
+    STS OCR1CH, R16
+    LDI R16, DEFAULT_COUNT & 0xFF
+    STS OCR1AL, R16
+    STS OCR1BL, R16
+    STS OCR1CL, R16
+
+; Mode 14: (WGM13, WGM12, WGM11, WGM10)=(1,1,1,0)
+; Clear on match, set at bottom: (COM1A1,COM1A0)=(1,0) (COM1B1,COM1B0)=(1,0) (COM1C1,COM1C0)=(1,0)
+    LDI R16, (1<<COM1A1) | (0<<COM1A0) | (1<<COM1B1) | (0<<COM1B0) | (1<<COM1C1) | (0<<COM1C0) | (1<<WGM11) | (0<<WGM10)
+    STS TCCR1A, R16
+
+; Mode 14: (WGM13, WGM12, WGM11, WGM10)=(1,1,1,0)
+; Clock select: (CS12,CS11,CS10)=(0,1,0), PRESCALER/8
+; No input capture: (0<<ICNC1) | (0<<ICES1)
+    LDI R16, (0<<ICNC1) | (0<<ICES1) | (1<<WGM13) | (1<<WGM12) | (0<<CS12) |(1<<CS11) | (0<<CS10)
+    STS TCCR1B, R16
+
+; Timer/Counter 1 Interrupt(s) initialization
+; No interrupt is needed to generate PWM
+    LDI R16, (0<<ICIE1) | (0<<OCIE1C) | (0<<OCIE1B) | (0<<OCIE1A) | (0<<TOIE1)
+    STS TIMSK1, R16
     RET
 
 ;*********************************************************************
