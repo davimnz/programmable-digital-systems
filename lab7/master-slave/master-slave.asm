8daf28e090557478c88d3df4eb9c5cc303c79a23
vasconcelosdavi <davi.muniz41@gmail.com>
2021-10-18 23:24:08 -0300

    fix errors

--- lab7/master-slave/master-slave.asm
+++ lab7/master-slave/master-slave.asm
@@ -7,7 +7,7 @@
 
    .EQU BAUD_RATE_57600 = 16   ; constant for 57600 baud rate
 
-   .EQU ENTER_KEY = 0x0a       ; enter ASCII code
+   .EQU ENTER_KEY = 0x0d       ; enter ASCII code
    .EQU S_KEY = 83             ; S ASCII code
    .EQU L_KEY = 76             ; L ASCII code
    .EQU PLUS_KEY = 43          ; plus sign ASCII code
@@ -53,8 +53,8 @@ RESET:
     CALL INIT_PORTF   ; sets F0 as output
     CALL INIT_PORTL   ; sets L7 as input
 	CALL INIT_PORTH   ; sets H0, H1 as output
-	CALL USART0_INIT
-	CALL USART1_INIT
+	CALL USART0_INIT  ; initializes usart 0 (terminal)
+	CALL USART1_INIT  ; initializes usart 1 (master - slave communication)
 
 GET_TYPE:
     LDS  R16, PINL
@@ -79,17 +79,23 @@ MASTER_LOOP:
 	CALL USART1_TRANSMIT
 
 	INC  R23
-    JMP  MASTER_LOOP
+    CPI  R16, ENTER_KEY
+	BREQ MASTER_END_WRITING
+	JMP  MASTER_LOOP
 
 MASTER_WAIT_ENTER:
     CALL USART0_RECEIVE
-	CPI  R16, 0x0D
-	BREQ MASTER_END_WRITING
+	CPI  R16, ENTER_KEY
+	BREQ MASTER_SEND_ENTER
 	JMP  MASTER_WAIT_ENTER
 
-MASTER_END_WRITING:
+MASTER_SEND_ENTER:
     CALL USART0_TRANSMIT
 	CALL USART1_TRANSMIT
+
+MASTER_END_WRITING:
+    ;CALL USART0_TRANSMIT
+	;CALL USART1_TRANSMIT
     LDI  R23, 0x00
 
 	CALL USART1_RECEIVE    ; receives character from slave and stores it in r16
@@ -116,9 +122,19 @@ SLAVE:
 	LDI  ZL, LOW(2*MSG_SLAVE)   ;
 	CALL SEND_MSG_USART0        ;
 
+SLAVE_INIT_REGISTERS:
+    LDI R18, 0xff
+	LDI R19, 0xff
+	LDI R20, 0xff
+	LDI R21, 0xff
+	LDI R22, 0xff
+
 SLAVE_READ_FIRST:               
     CALL USART1_RECEIVE         ; reads first character sent by master
 	MOV  R18, R16               ; r18 stores first letter of protocol
+
+	CPI  R18, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
 
 	CPI  R18, S_KEY             ; verifies if first letter is "S"
 	BREQ SLAVE_READ_SERVO       ; goes to servo protocol reading
@@ -126,28 +142,34 @@ SLAVE_READ_FIRST:
 	CPI  R18, L_KEY             ; verifies if first letter is "L"
 	BREQ SLAVE_READ_LED         ; goes to led protocol reading
 
-	JMP  SLAVE_UNSUCCESS_1   ; if component letter not valid
+	JMP  SLAVE_UNSUCCESS_1      ; if component letter not valid
 
 SLAVE_READ_SERVO:
-    CALL USART1_RECEIVE       ; reads character sent by master to r16 
-	MOV  R19, R16             ; r19 stores servo number
+    CALL USART1_RECEIVE         ; reads character sent by master to r16 
+	MOV  R19, R16               ; r19 stores servo number
 
-	CPI  R19, 0x30            ; verifies if servo number is zero
-	BREQ SLAVE_READ_SIGNAL    ;
+	CPI  R19, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
 
-	CPI  R19, 0x31            ; verifies if servo number is one
-	BREQ SLAVE_READ_SIGNAL    ; 
+	CPI  R19, 0x30              ; verifies if servo number is zero
+	BREQ SLAVE_READ_SIGNAL      ;
 
-	CPI  R19, 0x32            ; verifies if servo number is two
-	BREQ SLAVE_READ_SIGNAL    ;
+	CPI  R19, 0x31              ; verifies if servo number is one
+	BREQ SLAVE_READ_SIGNAL      ; 
 
-	JMP  SLAVE_UNSUCCESS_2    ; if servo number not valid
+	CPI  R19, 0x32              ; verifies if servo number is two
+	BREQ SLAVE_READ_SIGNAL      ;
+
+	JMP  SLAVE_UNSUCCESS_2      ; if servo number not valid
 
 SLAVE_READ_SIGNAL:
-	CALL USART1_RECEIVE                  ; reads character sent by master to r16
-	MOV  R20, R16                        ; r20 stores the sign
+	CALL USART1_RECEIVE         ; reads character sent by master to r16
+	MOV  R20, R16               ; r20 stores the sign
 
-	CPI  R20, PLUS_KEY                   ; verifies if sign is +
+	CPI  R20, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
+
+	CPI  R20, PLUS_KEY          ; verifies if sign is +
 	BREQ SLAVE_READ_ANGLE_FIRST_DIGIT    ;
 
 	CPI  R20, MINUS_KEY                  ; verifies if sign is -
@@ -159,6 +181,9 @@ SLAVE_READ_ANGLE_FIRST_DIGIT:
     CALL USART1_RECEIVE   ; reads character sent by master to r16
 	MOV  R21, R16
 
+	CPI  R21, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
+
 	CALL FIRST_DIGIT_IS_DIGIT
 
 	MOV  R24, R21         ; r24 stores the first digit of angle
@@ -168,16 +193,24 @@ SLAVE_READ_ANGLE_SECOND_DIGIT:
     CALL USART1_RECEIVE   ; reads character sent by master to r16
 	MOV  R22, R16
 
+	CPI  R22, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
+
 	CALL SECOND_DIGIT_IS_DIGIT
 
 	MOV  R25, R22         ; r25 stores the second digit of angle
 	SUBI R25, 0x30        ; subtracts zero ASCII code to get digit
 	JMP  SLAVE_SUCCESS_READ_ENTER
 
+SLAVE_UNSUCCESS_JMP:
+    JMP SLAVE_UNSUCCESS
 
 SLAVE_READ_LED:
     CALL USART1_RECEIVE
 	MOV  R19, R16
+
+	CPI  R19, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
 
 	CPI  R19, 0x30
 	BREQ SLAVE_READ_LED_STATUS_1
@@ -191,6 +224,9 @@ SLAVE_READ_LED_STATUS_1:
     CALL USART1_RECEIVE
     MOV  R20, R16
 
+	CPI  R20, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
+
     CPI  R20, O_KEY
     BREQ SLAVE_READ_LED_STATUS_2
 
@@ -199,6 +235,9 @@ SLAVE_READ_LED_STATUS_1:
 SLAVE_READ_LED_STATUS_2:
     CALL USART1_RECEIVE
     MOV  R21, R16
+
+	CPI  R21, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
 
     CPI  R21, F_KEY
     BREQ SLAVE_READ_LED_STATUS_OFF
@@ -212,23 +251,29 @@ SLAVE_READ_LED_STATUS_OFF:
     CALL USART1_RECEIVE
 	MOV  R22, R16
 
+	CPI  R22, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
+
 	CPI  R22, F_KEY
 	BREQ SLAVE_SUCCESS_READ_ENTER
 
-	JMP  SLAVE_UNSUCCESS
+	JMP  SLAVE_UNSUCCESS_READ_ENTER
 
 SLAVE_READ_LED_STATUS_ON:
     CALL USART1_RECEIVE
 	MOV  R22, R16
 
+	CPI  R22, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS_JMP
+
 	CPI  R22, N_KEY
 	BREQ SLAVE_SUCCESS_READ_ENTER
 
-	JMP  SLAVE_UNSUCCESS
+	JMP  SLAVE_UNSUCCESS_READ_ENTER
 
 SLAVE_SUCCESS_READ_ENTER:
     CALL USART1_RECEIVE
-	CPI  R16, 0x0D
+	CPI  R16, ENTER_KEY
 	BREQ SLAVE_SUCCESS
 	JMP  SLAVE_SUCCESS_READ_ENTER
 
@@ -244,7 +289,7 @@ SLAVE_SUCCESS:
 	CPI  R18, S_KEY
 	BREQ SLAVE_SUCCESS_SERVO  
 
-	JMP  SLAVE_READ_FIRST          ; goes back to read another input
+	JMP  SLAVE_INIT_REGISTERS          ; goes back to read another input
 
 SLAVE_SUCCESS_LED:
     CPI  R19, 0x30
@@ -265,14 +310,14 @@ SLAVE_LED_0_OFF:
 	LDI R17, 0b11111110
 	AND R16, R17
 	STS PORTH, R16
-	JMP SLAVE_READ_FIRST
+	JMP SLAVE_INIT_REGISTERS
 
 SLAVE_LED_0_ON:
     LDS R16, PORTH
 	LDI R17, 0b00000001
 	OR  R16, R17
     STS PORTH, R16
-	JMP SLAVE_READ_FIRST
+	JMP SLAVE_INIT_REGISTERS
 
 SLAVE_CHANGE_LED_1:
     CPI  R22, F_KEY
@@ -286,14 +331,14 @@ SLAVE_LED_1_OFF:
 	LDI R17, 0b11111101
 	AND R16, R17
 	STS PORTH, R16
-	JMP SLAVE_READ_FIRST
+	JMP SLAVE_INIT_REGISTERS
 
 SLAVE_LED_1_ON:
     LDS R16, PORTH
 	LDI R17, 0b00000010
 	OR  R16, R17
     STS PORTH, R16
-	JMP SLAVE_READ_FIRST
+	JMP SLAVE_INIT_REGISTERS
 
 SLAVE_SUCCESS_SERVO:
     CALL SLAVE_GET_COUNT       ; evaluates the count for the given angle
@@ -310,17 +355,17 @@ SLAVE_SUCCESS_SERVO:
 SLAVE_CHANGE_SERVO_0:          ; changes the OCR1 value of servo 0
     STS OCR1AH, R26            ;
     STS OCR1AL, R27            ;
-    JMP SLAVE_READ_FIRST       ; starts a new protocol reading
+    JMP SLAVE_INIT_REGISTERS   ; starts a new protocol reading
 
 SLAVE_CHANGE_SERVO_1:          ; changes the OCR1 value of servo 1
     STS OCR1BH, R26            ;
     STS OCR1BL, R27            ;
-    JMP SLAVE_READ_FIRST       ; starts a new protocol reading
+    JMP SLAVE_INIT_REGISTERS   ; starts a new protocol reading
 
 SLAVE_CHANGE_SERVO_2:          ; changes the OCR1 value of servo 2
     STS OCR1CH, R26            ;
     STS OCR1CL, R27            ;
-    JMP SLAVE_READ_FIRST       ; starts a new protocol reading
+    JMP SLAVE_INIT_REGISTERS   ; starts a new protocol reading
 
 ;************************************************************
 ; Subroutine GET_COUNT                                     **
@@ -409,21 +454,33 @@ SLAVE_UNSUCCESS_1:
     CALL USART1_RECEIVE
 	MOV  R19, R16
 
+	CPI  R19, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS
+
 SLAVE_UNSUCCESS_2:
     CALL USART1_RECEIVE
 	MOV  R20, R16
+
+	CPI  R20, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS
 
 SLAVE_UNSUCCESS_3:
     CALL USART1_RECEIVE
 	MOV  R21, R16
 
+	CPI  R21, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS
+
 SLAVE_UNSUCCESS_4:
     CALL USART1_RECEIVE
 	MOV  R22, R16
 
+	CPI  R22, ENTER_KEY
+	BREQ SLAVE_UNSUCCESS
+
 SLAVE_UNSUCCESS_READ_ENTER:
     CALL USART1_RECEIVE
-	CPI  R16, 0x0D
+	CPI  R16, ENTER_KEY
 	BREQ SLAVE_UNSUCCESS
 	JMP  SLAVE_UNSUCCESS_READ_ENTER
 
@@ -431,27 +488,48 @@ SLAVE_UNSUCCESS:
     LDI  R16, 0xff
 	CALL USART1_TRANSMIT
 	CALL SLAVE_SEND_RECEIVED_MSG
-	JMP SLAVE_READ_FIRST
+	JMP  SLAVE_INIT_REGISTERS
 
 SLAVE_SEND_RECEIVED_MSG:
     MOV  R16, R18
+	CPI  R16, 0xff
+	BREQ SLAVE_SEND_LF_CR
+	CPI  R16, ENTER_KEY
+	BREQ SLAVE_SEND_LF_CR
 	CALL USART0_TRANSMIT
 
 	MOV  R16, R19
+	CPI  R16, 0xff
+	BREQ SLAVE_SEND_LF_CR
+	CPI  R16, ENTER_KEY
+	BREQ SLAVE_SEND_LF_CR
 	CALL USART0_TRANSMIT
 
 	MOV  R16, R20
+	CPI  R16, 0xff
+	BREQ SLAVE_SEND_LF_CR
+	CPI  R16, ENTER_KEY
+	BREQ SLAVE_SEND_LF_CR
 	CALL USART0_TRANSMIT
 
 	MOV  R16, R21
+	CPI  R16, 0xff
+	BREQ SLAVE_SEND_LF_CR
+	CPI  R16, ENTER_KEY
+	BREQ SLAVE_SEND_LF_CR
 	CALL USART0_TRANSMIT
 
 	MOV  R16, R22
+	CPI  R16, 0xff
+	BREQ SLAVE_SEND_LF_CR
+	CPI  R16, ENTER_KEY
+	BREQ SLAVE_SEND_LF_CR
 	CALL USART0_TRANSMIT
 
-	LDI  R16, 0x0A
+SLAVE_SEND_LF_CR:
+	LDI  R16, 0x0a
 	CALL USART0_TRANSMIT
-	LDI  R16, 0X0D
+	LDI  R16, 0X0d
 	CALL USART0_TRANSMIT
 	RET
 
@@ -519,7 +597,7 @@ SECOND_DIGIT_IS_DIGIT:
     CPI  R16, 0x39         ; verifies if r16 is '9'
     BREQ DIGIT_TRUE        ;
 
-    JMP  SLAVE_UNSUCCESS   ; goes to unsuccess protocol reading
+    JMP  SLAVE_UNSUCCESS_READ_ENTER ; goes to unsuccess protocol reading
 
 DIGIT_TRUE: ; returns to the next step of protocol reading
     RET     ; 
